858f2d5071bf92380955515245c682dfa749b728
Acropolis <stupidnamethingie@gmail.com>
2018-05-07 21:00:43 -0700

    Adding Image Retrieval capabilities.

--- src/main/java/com/knightowlgames/proxypal/service/CollectionService.java
+++ src/main/java/com/knightowlgames/proxypal/service/CollectionService.java
@@ -3,14 +3,16 @@ package com.knightowlgames.proxypal.service;
 import com.knightowlgames.proxypal.datatype.MagicCard;
 import com.knightowlgames.proxypal.repository.CollectionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CollectionService {
 
     @Autowired
     private CollectionRepository collectionRepository;
 
-    public saveCard(MagicCard magicCard)
+    public MagicCard saveCard(MagicCard magicCard)
     {
-
+        return null;
     }
 }
